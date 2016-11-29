475cb4dc5b8ccb9e239ebe5cf7a5b8da7b148e4a
Ondrej Danko <xjcook@gmail.com>
2016-11-29 20:34:04 +0100

    Implement OAuth2 authentication (2/3) Add LoginFragment with WebView

---
+++ app/src/main/java/io/xjhub/gitview/LoginFragment.java
@@ -0,0 +1,107 @@
+package io.xjhub.gitview;
+
+import android.app.Fragment;
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.CookieManager;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+
+public class LoginFragment extends Fragment {
+
+    public static final String LOG_TAG = "LoginFragment";
+    public static final String EXTRA_AUTH_URL = "authUrl";
+    public static final String EXTRA_STATE = "state";
+
+    private Button mButton;
+    private WebView mWebView;
+
+    OnSignedInListener mCallback;
+
+    // Container Activity must implement this interface
+    public interface OnSignedInListener {
+        void onSignedIn(String code);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_login, container, false);
+
+        mButton = (Button) rootView.findViewById(R.id.loginBtn);
+        mWebView = (WebView) rootView.findViewById(R.id.webView);
+
+        mWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+
+                String cookies = CookieManager.getInstance().getCookie(url);
+                String code = getCookie(cookies, "code");
+                String state = getCookie(cookies, "state");
+
+                Log.d(LOG_TAG, "Cookie code:" + code);
+                Log.d(LOG_TAG, "Cookie state:" + state);
+
+                // Callback MainActivity if cookie is correct
+                if (code != null && state.equals(getArguments().getString(EXTRA_STATE))) {
+                    mCallback.onSignedIn(code);
+                }
+            }
+        });
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Go to authorization url
+                mWebView.loadUrl(getArguments().getString(EXTRA_AUTH_URL));
+
+                mButton.setVisibility(Button.GONE);
+                mWebView.setVisibility(WebView.VISIBLE);
+            }
+        });
+
+        return rootView;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        // This makes sure that the container activity has implemented
+        // the callback interface. If not, it throws an exception
+        try {
+            mCallback = (OnSignedInListener) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString()
+                    + " must implement OnSignedInListener");
+        }
+    }
+
+    /**
+     * Get cookie value
+     * @param cookies value from CookieManager.getCookie()
+     * @param cookieName name of the cookie
+     * @return cookieValue
+     */
+    public static String getCookie(String cookies, String cookieName) {
+        String cookieValue = null;
+
+        if (cookies != null) {
+            String[] temp = cookies.split(";");
+            for (String ar1 : temp) {
+                if (ar1.contains(cookieName)) {
+                    String[] temp1 = ar1.split("=");
+                    cookieValue = temp1[1];
+                }
+            }
+        }
+
+        return cookieValue;
+    }
+}
--- app/src/main/java/io/xjhub/gitview/MainActivity.java
+++ app/src/main/java/io/xjhub/gitview/MainActivity.java
@@ -1,9 +1,7 @@
 package io.xjhub.gitview;
 
 import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
-import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -16,60 +14,63 @@ import com.github.scribejava.core.oauth.OAuth20Service;
 import java.io.IOException;
 import java.util.Random;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoginFragment.OnSignedInListener {
 
     public static final String LOG_TAG = "MainActivity";
     public static final String CODE_PREF_KEY = "codeKey";
 
     private static final String CLIENT_ID = "ff406479db68276af5a3";
     private static final String CLIENT_SECRET = "0dbd87cbc9b6a3bb9cec5a8d4414d2ce1a4b19df";
-    private static final String CALLBACK_URL = "gitview://oauthresponse";
+    private static final String CALLBACK_URL = "http://gitview.xjhub.io/oauth";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
-        String code = sharedPref.getString(CODE_PREF_KEY, null);
+//        if (savedInstanceState != null) {
+            SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+            String code = sharedPref.getString(CODE_PREF_KEY, null);
 
-        final String secretState = "secret" + new Random().nextInt(999_999);
-        final OAuth20Service service = new ServiceBuilder()
-                .apiKey(CLIENT_ID)
-                .apiSecret(CLIENT_SECRET)
-                .state(secretState)
-                .callback(CALLBACK_URL)
-                .build(GitHubApi.instance());
+            final String secretState = "secret" + new Random().nextInt(999_999);
+            final OAuth20Service service = new ServiceBuilder()
+                    .apiKey(CLIENT_ID)
+                    .apiSecret(CLIENT_SECRET)
+                    .state(secretState)
+                    .callback(CALLBACK_URL)
+                    .build(GitHubApi.instance());
 
-        OAuth2AccessToken accessToken = null;
-        if (code != null) {
-            try {
-                accessToken = service.getAccessToken(code);
-            } catch (IOException e) {
-                Log.getStackTraceString(e);
-            }
-        }
+            OAuth2AccessToken accessToken = null;
+            /*if (code != null) {
+                try {
+                    accessToken = service.getAccessToken(code);
+                } catch (IOException e) {
+                    Log.getStackTraceString(e);
+                }
+            }*/
 
-        if (accessToken == null) {
-            Intent intent = new Intent(Intent.ACTION_VIEW,
-                    Uri.parse(service.getAuthorizationUrl()));
-            startActivity(intent);
-        }
+//            if (accessToken == null) {
+                // Show LoginFragment
+                Bundle args = new Bundle();
+                args.putString(LoginFragment.EXTRA_AUTH_URL, service.getAuthorizationUrl());
+                args.putString(LoginFragment.EXTRA_STATE, secretState);
+
+                LoginFragment loginFragment = new LoginFragment();
+                loginFragment.setArguments(args);
+
+                getFragmentManager().beginTransaction()
+                        .add(R.id.fragment_container, loginFragment).commit();
+//            }
+//        }
     }
 
     @Override
-    protected void onNewIntent(Intent intent) {
-        Uri uri = intent.getData();
+    public void onSignedIn(String code) {
+        Log.d(LOG_TAG, "Cookie code:" + code);
 
-        if (uri != null && uri.toString().startsWith(CALLBACK_URL)) {
-            String code = uri.getQueryParameter("access_token");
-            Log.d(LOG_TAG, code);
-
-            // Save code into shared preferences
-            SharedPreferences.Editor editor = getPreferences(Context.MODE_PRIVATE).edit();
-            editor.putString(CODE_PREF_KEY, code);
-            editor.apply();
-        }
+        // Save code into shared preferences
+        SharedPreferences.Editor editor = getPreferences(Context.MODE_PRIVATE).edit();
+        editor.putString(CODE_PREF_KEY, code);
+        editor.apply();
     }
-
 }
